bf412ec6a6723dc2145074dd3d84b7308cc16267
Ihor <ihorfront88@gmail.com>
2023-01-22 20:27:08 +0200

    add select and styles

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react';
+import { CustomSelect } from 'components/CustomSelect/CustomSelect.styled';
+import { useState, useEffect } from 'react';
 import { Search } from './../Search/Search';
+import { Wrapper } from './../Controls/Controls.styled';
 
-export const Controls = () => {
+const options = [
+  { value: 'Africa', label: 'Africa' },
+  { value: 'America', label: 'America' },
+  { value: 'Asia', label: 'Asia' },
+  { value: 'Europe', label: 'Europe' },
+  { value: 'Oceania', label: 'Oceania' },
+];
+
+export const Controls = ({ onSearch }) => {
   const [search, setSearch] = useState('');
+  const [region, setRegion] = useState('');
+
+  // useEffect(() => {
+  //   const regionValue = region?.value || '';
+  //   onSearch(search, regionValue);
+
+  //   // eslint-disable-next-line
+  // }, [search, region]);
 
   return (
-    <div>
+    <Wrapper>
       <Search search={search} setSearch={setSearch} />
-    </div>
+      <CustomSelect
+        options={options}
+        placeholder="Filter by Region"
+        isClearable
+        isSearchable={false}
+        value={region}
+        onChange={setRegion}
+      />
+    </Wrapper>
   );
 };
--- src/components/Search/Search.styled.js
+++ src/components/Search/Search.styled.js
@@ -5,12 +5,10 @@ export const InputLabel = styled.label`
   padding: 1rem 2rem;
   display: flex;
   align-items: center;
-
   border-radius: var(--radii);
   box-shadow: var(--shadow);
   width: 100%;
-  margin-bottom: 1.5rem;
-
+  margin-bottom: 1rem;
   @media (min-width: 767px) {
     margin-bottom: 0;
     width: 280px;
@@ -25,4 +23,5 @@ export const Input = styled.input.attrs({
   border: none;
   outline: none;
   color: var(--color-text);
+  background-color: var(--colors-ui-base);
 `;
